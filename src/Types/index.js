883c2a9cdfa22089e10e473112ca05a9f9c5ae4c
BrunoEmaSN <sanchezbruno689@gmail.com>
2022-03-01 22:39:26 -0300

    refactoring types.js

--- src/Types/index.js
+++ src/Types/index.js
@@ -1,17 +1,49 @@
 export const types = {
+    // Aula
+    aulas: '[Aula]',
+
     // Alumno
-    alumnosGetAll: '[Alumno] Get All alumnos',
-    alumnosGetOne: '[Alumno] Get One alumno',
-    alumnosAdd: '[Alumno] New Alumno',
-    alumnosUpdate: '[Alumno] Update Alumno',
-    alumnosRemove: '[Alumno] Remove Alumno',
-    alumnosIncribirCurso: '[Alumno] Inscribir Curso',
-    alumnosAddTutor: '[Alumno] Add Tutor',
+    alumnos: '[Alumno]',
+
+    // Calificacion
+    calificaciones: '[Calificacion]',
+
+    // Clase
+    clases: '[Clase]',
+
+    // Curso
+    cursos: '[Curso]',
+
+    // Docente
+    docentes: '[Docente]',
+
+    // Materia
+    materias: '[Materia]',
+
+    // Mesa Examen
+    mesasExamenes: '[Mesa Examen]',
+
+    // Sancion
+    sanciones: '[Sancion]',
+
+    // Tutor
+    tutores: '[Tutor]',
+
+    // Usuario
+    usuariosLogin: '[Usuario] Login',
 
     // UI
     uiSetError: '[UI] Set Error',
     uiRemoveError: '[UI] Remove Error',
     uiStartLoading: '[UI] Start loading',
-    uiFinishLoading: '[UI] Finish loading'
+    uiFinishLoading: '[UI] Finish loading',
 
+    // Types Generics
+    getAll: 'Get All',
+    getOne: 'Get One',
+    add: 'Add New',
+    update: 'Update',
+    remove: 'remove',
+    active: 'Active',
+    cleanActive: 'Clean Active',
 };
