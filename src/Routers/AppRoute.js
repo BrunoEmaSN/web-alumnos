ca6e798370ad0629b2663c55e279a6fcb0ce35fc
BrunoEmaSN <sanchezbruno689@gmail.com>
2022-03-01 22:44:30 -0300

    modifiying routes

--- src/Routers/AppRoute.js
+++ src/Routers/AppRoute.js
@@ -5,25 +5,68 @@ import {
     Routes,
     Navigate
 } from 'react-router-dom';
-import { AlumnosSave } from '../Pages/Alumnos/AlumnosSave';
 import { AlumnosScreen } from '../Pages/Alumnos/AlumnosScreen';
+import { AulasScreen } from '../Pages/Aulas/AulasScreen';
+import { CalificacionesScreen } from '../Pages/Calificaciones/CalificacionesScreen';
+import { ClasesScreen } from '../Pages/Clases/ClasesScreen';
+import { CursosScreen } from '../Pages/Cursos/CursosScreen';
+import { DocentesScreen } from '../Pages/Docentes/DocentesScreen';
 import { HomeScreen } from '../Pages/Home/HomeScreen';
+import { MateriasScreen } from '../Pages/Materias/MatriasScreen';
+import { MesasExamenesScreen } from '../Pages/MesasExamenes/MesasExamenesScreen';
+import { SancionesScreen } from '../Pages/Sanciones/SancionesScreen';
+import { TutoresScreen } from '../Pages/Tutores/TutoresScreen';
 
 export const AppRoute = () => {
     return (
         <Router>
             <div>
                 <Routes>
-                    <Route exact path="/Alumnos/Save" element={
-                        <AlumnosSave />
-                    } />
+                    <Route exact path="/Aulas" element={
+                        <AulasScreen />
+                    }/>
+                    
                     <Route exact path="/Alumnos" element={
                         <AlumnosScreen />
                     }/>
+
+                    <Route exact path="/Calificaciones" element={
+                        <CalificacionesScreen />
+                    }/>
+
+                    <Route exact path="/Clases" element={
+                        <ClasesScreen />
+                    }/>
+
+                    <Route exact path="/Cursos" element={
+                        <CursosScreen />
+                    }/>
+
+                    <Route exact path="/Docentes" element={
+                        <DocentesScreen />
+                    }/>
+
+                    <Route exact path="/Materias" element={
+                        <MateriasScreen />
+                    }/>
+
+                    <Route exact path="/MesasExamenes" element={
+                        <MesasExamenesScreen />
+                    }/>
+
+                    <Route exact path="/Sanciones" element={
+                        <SancionesScreen />
+                    }/>
+
+                    <Route exact path="/Tutores" element={
+                        <TutoresScreen />
+                    }/>
+
                     <Route exact path="/" element={
                         <HomeScreen />
                     }/>
-                    <Route path="*" element={<Navigate to ="/" />}/>
+                    
+                    <Route path="*" element={ <Navigate to ="/" /> }/>
                 </Routes>
             </div>
         </Router>
