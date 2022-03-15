1ac5dc93651a7e4c10295fd3992e51a055b185c4
BrunoEmaSN <sanchezbruno689@gmail.com>
2022-03-15 09:36:30 -0300

    modified restSanciones

--- src/Services/restCallSanciones.js
+++ src/Services/restCallSanciones.js
@@ -13,17 +13,7 @@ export const sancionesGetOne = async ( id ) => {
     return await axiosWithToken( url );
 }
 
-export const sancionesAdd = async ( data ) => {
-    const url = `${ baseUrl }/sanciones/`;
-    return await axiosWithToken( url, data, 'POST' );
-}
-
 export const sancionesUpdate = async ( id, data ) => {
     const url = `${ baseUrl }/sanciones/id/${ id }`;
     return await axiosWithToken( url, data, 'PUT' );
-}
-
-export const sancionesDelete = async ( id ) => {
-    const url = `${ baseUrl }/sanciones/id/${ id }`;
-    return await axiosWithToken( url, {}, 'DELETE' );
 }
