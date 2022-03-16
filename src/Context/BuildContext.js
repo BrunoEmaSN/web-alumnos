4b0827841a4856f7d13b9a470fe2c9fcbdb18798
BrunoEmaSN <sanchezbruno689@gmail.com>
2022-03-16 12:01:57 -0300

    create all context

---
+++ src/Context/BuildContext.js
@@ -0,0 +1,12 @@
+import { createContext } from "react";
+
+export const AlumnosContext = createContext();
+export const AulasContext = createContext();
+export const CalificacionesContext = createContext();
+export const ClasesContext = createContext();
+export const CursosContext = createContext();
+export const DocentesContext = createContext();
+export const MateriasContext = createContext();
+export const MesasExamenesContext = createContext();
+export const SancionesContext = createContext();
+export const TutoresContext = createContext();
